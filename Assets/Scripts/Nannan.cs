5ce4e9975457b9c95ec3fc1c00ad5cb538a336b5
kaitlyncliu <liu.kaitlync@gmail.com>
2022-03-18 17:41:57 -0400

    maze integration

--- Assets/Scripts/Nannan.cs
+++ Assets/Scripts/Nannan.cs
@@ -17,23 +17,24 @@ public class Nannan : MonoBehaviour
     void Update()
     {
    
-        if(Input.GetKey(KeyCode.LeftArrow))
+        if(Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
         {
+            Debug.Log("left");
             transform.Translate(-speed * Time.deltaTime, 0, 0);
 
             
         }
-        if (Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
         {
             transform.Translate(speed * Time.deltaTime, 0, 0);
             
         }
-        if (Input.GetKey(KeyCode.UpArrow))
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
         {
             transform.Translate(0, speed * Time.deltaTime, 0);
             
         }
-        if (Input.GetKey(KeyCode.DownArrow))
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
         {
             transform.Translate(0, -speed * Time.deltaTime, 0);
             
