90f3929a1d4394abf8289bf1269c29421b99fbf2
charviahoy <charviahoy@gmail.com>
2022-03-31 17:00:00 -0400

    update inventory.cs

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -9,6 +9,8 @@ public class Inventory : MonoBehaviour
 
     public void Start(){
         GiveItem(0);
+        GiveItem("Bear");
+        RemoveItem(0);
     }
 
     public void GiveItem(int id){
@@ -17,4 +19,25 @@ public class Inventory : MonoBehaviour
         Debug.Log("added item: " + itemToAdd.title);
 
     }
+
+    public void GiveItem(string itemName){
+        Item itemToAdd = itemDatabase.GetItem(itemName);
+        characterItems.Add(itemToAdd);
+        Debug.Log("added item: " + itemToAdd.title);
+
+    }
+
+    public Item CheckForItem(int id){
+        return characterItems.Find(item => item.id == id);
+    }
+
+    public void RemoveItem(int id){
+        Item item = CheckForItem(id);
+        if (item != null){
+            characterItems.Remove(item);
+            Debug.Log("Item removed: " + item.title);
+        }
+    }
+
+
 }
