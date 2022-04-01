858f30c4cc533d87350ff6afe1dbea5d67afeeac
kaitlyncliu <liu.kaitlync@gmail.com>
2022-04-01 15:28:06 -0400

    Update itemPickup.cs

--- Assets/Scripts/itemPickup.cs
+++ Assets/Scripts/itemPickup.cs
@@ -6,13 +6,13 @@ public class itemPickup : MonoBehaviour
 {
     public Texture2D texture;
     public Inventory inv;
-    public SpriteRenderer spr;
 
 
     // Start is called before the first frame update
     void OnMouseDown(){
         Cursor.SetCursor(null,Vector2.zero,CursorMode.Auto);
         inv.GiveItem(gameObject.name);
+        gameObject.SetActive(false);
     }
 
     void OnMouseOver(){
