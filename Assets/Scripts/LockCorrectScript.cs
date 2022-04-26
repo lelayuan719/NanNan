f2f4f45cadb0675b140167f07be8a2f64c4b1bb1
Emily Guo <LilyCaroline1717@gmail.com>
2022-04-26 13:20:38 -0400

    Added display screen to Password Puzzle to display numbers the user enters
    Updated Password Puzzle with a Display Screen.

--- Assets/Scripts/LockCorrectScript.cs
+++ Assets/Scripts/LockCorrectScript.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LockCorrectScript : MonoBehaviour
 {
     public bool isSolved;
     private string codeSequence;
     private int codeLength;
+    [SerializeField]
+    private Sprite[] digits;
+    [SerializeField]
+    private Image[] numbers;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +19,9 @@ public class LockCorrectScript : MonoBehaviour
         codeSequence = "";
         codeLength = 7;
         PushButtonScript.ButtonPressed += AddDigitToSequence;
+        for (int i = 0; i < numbers.Length; i++){
+            numbers[i].sprite = digits[9];
+        }
     }
 
     private void AddDigitToSequence(string digit){
@@ -21,39 +29,39 @@ public class LockCorrectScript : MonoBehaviour
             switch (digit){
                 case "One":
                     codeSequence += "1";
-                    Debug.Log("1");
+                    Display(1);
                     break;
                 case "Two":
                     codeSequence += "2";
-                    Debug.Log("2");
+                    Display(2);
                     break;
                 case "Three":
                     codeSequence += "3";
-                    Debug.Log("3");
+                    Display(3);
                     break;
                 case "Four":
                     codeSequence += "4";
-                    Debug.Log("4");
+                    Display(4);
                     break;
                 case "Five":
                     codeSequence += "5";
-                    Debug.Log("5");
+                    Display(5);
                     break;
                 case "Six":
                     codeSequence += "6";
-                    Debug.Log("6");
+                    Display(6);
                     break;
                 case "Seven":
                     codeSequence += "7";
-                    Debug.Log("7");
+                    Display(7);
                     break;
                 case "Eight":
                     codeSequence += "8";
-                    Debug.Log("8");
+                    Display(8);
                     break;
                 case "Nine":
-                    codeSequence += "9";
-                    Debug.Log("9");
+                    codeSequence += "9"; 
+                    Display(9);
                     break;
             }
         }
@@ -70,7 +78,7 @@ public class LockCorrectScript : MonoBehaviour
             Debug.Log("Correct!");
             isSolved = true;
         }
-        else{
+        else {
             Debug.Log("Incorrect!");
             ClearCode();
         }
@@ -78,7 +86,14 @@ public class LockCorrectScript : MonoBehaviour
 
     private void ClearCode(){
         Debug.Log("Cleared");
+        for (int i = 0; i < numbers.Length; i++){
+            numbers[i].sprite = digits[9];
+        }
         codeSequence = "";
+    }
+
+    private void Display(int recentDigit){
+        numbers[codeSequence.Length-1].sprite = digits[recentDigit-1];
     }
 
     /*private void OnDestroy(){
