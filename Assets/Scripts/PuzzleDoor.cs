1574152ae8f199d3a61ea0d01f0df81c602cfdb2
kaitlyncliu <liu.kaitlync@gmail.com>
2022-04-28 21:28:48 -0400

    add bear in ch1 room

--- Assets/Scripts/PuzzleDoor.cs
+++ Assets/Scripts/PuzzleDoor.cs
@@ -1,30 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class PuzzleDoor : MonoBehaviour
 {
     private bool success;
+    public Sprite openDoor;
     public GameObject tiger;
+    private bool opened;
     // Start is called before the first frame update
     void Start()
     {
         tiger.SetActive(false);
-        success = gameObject.GetComponent<ItemMatch>().success;
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+        success = gameObject.GetComponent<ItemMatch>().success;
+        if(success && !opened){
+            gameObject.GetComponent<SpriteRenderer>().sprite = openDoor;
+        }
     }
     private void OnMouseDown()
     {
         success = gameObject.GetComponent<ItemMatch>().success;
         if (success)
         {
-            //print("tiger doll trigger");
-            tiger.SetActive(true);
+            Debug.Log("to room");
+            SceneManager.LoadScene("chapter_1_room",LoadSceneMode.Single);
         }
     }
    
