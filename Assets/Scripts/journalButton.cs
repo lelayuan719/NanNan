28893bfe6f34dd82f5da6f8d17175126376d3f52
charviahoy <charviahoy@gmail.com>
2022-04-29 17:53:56 -0400

    inventory finsihed

---
+++ Assets/Scripts/journalButton.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class journalButton : MonoBehaviour
+{
+    public GameObject otherButton1;
+    public GameObject otherButton2;
+    public GameObject otherPanel1;
+    public GameObject otherPanel2;
+    public GameObject panel;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void openPanel()
+    {
+        if(otherPanel1.activeSelf){
+            otherPanel1.SetActive(false);
+        } else if(otherPanel2.activeSelf){
+            otherPanel2.SetActive(false);
+        }
+
+        panel.SetActive(true);
+        
+    }
+}
