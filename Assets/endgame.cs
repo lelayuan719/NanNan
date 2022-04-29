bc3b960cab6facaa71345605954eaa250edca28e
kaitlyncliu <liu.kaitlync@gmail.com>
2022-04-28 22:25:54 -0400

    doors puzzles

--- Assets/endgame.cs
+++ Assets/endgame.cs
@@ -3,33 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-
-public class PuzzleDoor : MonoBehaviour
+public class endgame : MonoBehaviour
 {
     private bool success;
     public Sprite openDoor;
+    public GameObject tiger;
     private bool opened;
     // Start is called before the first frame update
     void Start()
     {
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        success = gameObject.GetComponent<ItemMatch>().success;
-        if(success && !opened){
-            gameObject.GetComponent<SpriteRenderer>().sprite = openDoor;
-        }
     }
+
     private void OnMouseDown()
     {
         success = gameObject.GetComponent<ItemMatch>().success;
         if (success)
         {
             Debug.Log("to room");
-            SceneManager.LoadScene("chapter_1_room",LoadSceneMode.Single);
+            SceneManager.LoadScene("menu",LoadSceneMode.Single);
         }
     }
-   
 }
