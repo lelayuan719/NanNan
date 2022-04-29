0faa0895a4adb4d2d2dd545bf4793656265246b2
Emily Guo <LilyCaroline1717@gmail.com>
2022-04-29 15:59:58 -0400

    Lock Nannan in Room and changed door cursor

--- Assets/Scripts/OpenRoomOnClick.cs
+++ Assets/Scripts/OpenRoomOnClick.cs
@@ -11,6 +11,7 @@ public class OpenRoomOnClick : MonoBehaviour
     //public GameObject pauseRootScene;
     // public GameObject Room;
     //private bool opened;
+    public Texture2D texture;
     public GameObject Nannan;
 
     // Start is called before the first frame update
@@ -22,8 +23,7 @@ public class OpenRoomOnClick : MonoBehaviour
     void OnMouseDown()
     {
         Nannan.transform.position = new Vector3(-5.8f, -1.98f, 0f);
-        // Room.SetActive(true);
-        //pauseRootScene.SetActive(false);
+        Cursor.SetCursor(null,Vector2.zero,CursorMode.Auto);
     }
     public void CloseRoom()
     {
@@ -42,10 +42,10 @@ public class OpenRoomOnClick : MonoBehaviour
 
     void OnMouseOver()
     {
-        //Cursor.SetCursor(texture, Vector2.zero, CursorMode.Auto);
+        Cursor.SetCursor(texture,Vector2.zero,CursorMode.Auto);
     }
     void OnMouseExit()
     {
-        //Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+        Cursor.SetCursor(null,Vector2.zero,CursorMode.Auto);
     }
 }
