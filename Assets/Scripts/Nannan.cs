923f3312698d2f5c178a8c6e08736635e449bb25
Eli-bassoon <61474669+Eli-bassoon@users.noreply.github.com>
2022-09-25 15:10:10 -0400

    fix nannan maze controller stuttering

--- Assets/Scripts/Nannan.cs
+++ Assets/Scripts/Nannan.cs
@@ -7,56 +7,53 @@ public class Nannan : MonoBehaviour
     public int score = 0;
     public float speed = 5.0f;
     public GameObject door;
+
+    private Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-   
-        if(Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
-        {
-            Debug.Log("left");
-            transform.Translate(-speed * Time.deltaTime, 0, 0);
+        rb.velocity = Vector2.zero;
 
-            
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        {
+            rb.velocity += new Vector2(-speed, 0);
         }
         if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
         {
-            transform.Translate(speed * Time.deltaTime, 0, 0);
-            
+            rb.velocity += new Vector2(speed, 0);
         }
         if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
         {
-            transform.Translate(0, speed * Time.deltaTime, 0);
-            
+            rb.velocity += new Vector2(0, speed);
         }
         if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
         {
-            transform.Translate(0, -speed * Time.deltaTime, 0);
-            
+            rb.velocity += new Vector2(0, -speed);
         }
+
         if (score == 4)
         {
             Destroy(door);
         }
-
-
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.tag == "Keys")
+        if (collision.gameObject.tag == "Keys")
         {
             score++;
             Destroy(collision.gameObject);
         }
 
-        if(collision.gameObject.tag == "Walls")
+        if (collision.gameObject.tag == "Walls")
         {
-            
+
         }
 
 
