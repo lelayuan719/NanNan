aa484398c97fe5c25e78b5c1894ea83d8e13bdda
Eli-bassoon <61474669+Eli-bassoon@users.noreply.github.com>
2022-09-26 20:47:22 -0400

    Prologue minimap

---
+++ Assets/Scripts/Prologue/PologueMinimap.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PologueMinimap : MonoBehaviour
+{
+    private float startX;
+    private float length = 4720.751f;
+    private int rotations = 0;
+
+    public Transform character;
+
+    void Start()
+    {
+        startX = character.position.x;
+    }
+
+    void Update()
+    {
+        float rotateAmount = (character.position.x - startX) / length;
+        if (rotateAmount >= 1+rotations) {
+            rotations++;
+            Debug.LogFormat("Completed {0} rotation(s)", rotations);
+        }
+
+        transform.eulerAngles = new Vector3(0, 0, 180 + 360 * rotateAmount);
+    }
+}
