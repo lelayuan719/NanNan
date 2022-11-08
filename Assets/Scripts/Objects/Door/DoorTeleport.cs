cf600b10c7d458abf211d54f5d5b7e50fd03eb77
Eli-bassoon <61474669+Eli-bassoon@users.noreply.github.com>
2022-11-08 00:35:38 -0500

    Door teleportation raycast fix

--- Assets/Scripts/Objects/Door/DoorTeleport.cs
+++ Assets/Scripts/Objects/Door/DoorTeleport.cs
@@ -11,9 +11,13 @@ public class DoorTeleport : MonoBehaviour
     public CinemachineVirtualCamera cam;
     public Transform destination;
 
+    private Vector3 destPos;
+
     // Start is called before the first frame update
     private void Start()
     {
+        // This makes sure we can still raycast through NanNan
+        destPos = new Vector3(destination.position.x, destination.position.y, 1);
     }
 
     void OnMouseDown()
@@ -26,8 +30,8 @@ public class DoorTeleport : MonoBehaviour
 
     public void TransportPlayer()
     {
-        cam.OnTargetObjectWarped(player.transform, destination.position - player.transform.position);
-        player.transform.position = destination.position;
+        cam.OnTargetObjectWarped(player.transform, destPos - player.transform.position);
+        player.transform.position = destPos;
     }
     
     public void Unlock()
