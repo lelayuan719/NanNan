57214a4cdfb70e5271eddfd016887075d5ea8059
Eli-bassoon <61474669+Eli-bassoon@users.noreply.github.com>
2022-11-08 00:31:25 -0500

    Chapter 1 -> 2 transition

--- Assets/Scripts/EventInterface.cs
+++ Assets/Scripts/EventInterface.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class EventInterface : MonoBehaviour
 {
@@ -12,5 +13,10 @@ public class EventInterface : MonoBehaviour
     public void RemoveItem(string name)
     {
         GameManager.GM.inventory.RemoveItem(name);
+    }
+
+    public void ChangeScene(string name)
+    {
+        SceneManager.LoadScene(name, LoadSceneMode.Single);
     }
 }
