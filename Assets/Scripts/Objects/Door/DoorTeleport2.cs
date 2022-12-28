8bb911f19230218ddb465c6370e9249035ac22d3
Eli-bassoon <61474669+Eli-bassoon@users.noreply.github.com>
2022-12-28 04:49:49 -0500

    Improve door teleportation

--- Assets/Scripts/Objects/Door/DoorTeleport2.cs
+++ Assets/Scripts/Objects/Door/DoorTeleport2.cs
@@ -11,21 +11,46 @@ public class DoorTeleport2 : MonoBehaviour
     [SerializeField] CinemachineVirtualCamera endCam;
 
     private Vector3 destPos;
+    private SimpleTransitionSameScene transition;
 
-    private void Start()
+    private void Awake()
     {
-        destPos = destination.position;
-        destPos.z = 1;
+        transition = GetComponent<SimpleTransitionSameScene>();
     }
 
     void OnMouseDown()
     {
         if (opened && GameManager.GM.player.GetComponent<GenericController>().playerCanMove)
-        { 
-            GameManager.GM.player.transform.position = destPos;
-            GameManager.GM.cine.ActiveVirtualCamera.VirtualCameraGameObject.SetActive(false);
-            endCam.gameObject.SetActive(true);
+        {
+            transition.Transition();
         }
+    }
+
+    public void Teleport()
+    {
+        GetDestPos();
+
+        GameManager.GM.player.transform.position = destPos;
+        GameManager.GM.cine.ActiveVirtualCamera.VirtualCameraGameObject.SetActive(false);
+        endCam.gameObject.SetActive(true);
+    }
+
+    void GetDestPos()
+    {
+        float halfHeight = GameManager.GM.player.GetComponent<BoxCollider2D>().size.y * GameManager.GM.player.transform.lossyScale.y / 2;
+        Vector2 halfHeightOffset = new Vector2(0, halfHeight);
+
+        var raycasts = Physics2D.RaycastAll(destination.position, Vector2.down, 4);
+        foreach(var raycast in raycasts)
+        {
+            if (raycast.transform.gameObject.name == "Ground")
+            {
+                destPos = raycast.point + halfHeightOffset;
+                break;
+            }
+        }
+
+        destPos.z = 1;
     }
 
     public void Unlock()
