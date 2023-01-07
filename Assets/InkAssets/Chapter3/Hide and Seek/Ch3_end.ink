577d50c1bfb9d152b94a44802e36292fd0cf35e6
Eli-bassoon <61474669+Eli-bassoon@users.noreply.github.com>
2023-01-07 04:51:59 -0500

    Playable chapter all the way through
    Definitely not finished

--- Assets/InkAssets/Chapter3/Hide and Seek/Ch3_end.ink
+++ Assets/InkAssets/Chapter3/Hide and Seek/Ch3_end.ink
@@ -1,4 +1,4 @@
-Xiaoying: I can finally go in peace. Thank you, NanNan.
+Xiaoying: I can finally go in peace. Thank you, NanNan. #xiaoyingFade
 NanNan: She took the necklace, too.
 NanNan: I’m so tired... I want to go home...
 Mr. Li: NanNan, go home and don’t say a word to your family. I’ll take care of the mess.
--- Assets/Scripts/SceneLoading/SceneLoader.cs
+++ Assets/Scripts/SceneLoading/SceneLoader.cs
@@ -81,7 +81,7 @@ public class SceneLoader : MonoBehaviour
         else if (transition.transitionType == SceneTransitionSettings.TransitionType.Event)
         {
             onTransition.Invoke();
-            if (unfreeze) GameManager.GM.player.GetComponent<GenericController>().playerCanMove = true;
+            if (unfreeze) GameManager.GM.player.GetComponent<GenericController>().UnfreezeCharacter();
             ContinueLoad();
         }
         else
@@ -133,7 +133,7 @@ public class SceneLoader : MonoBehaviour
         yield return SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Single);
 
         Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
-        if (unfreeze) GameManager.GM.player.GetComponent<GenericController>().UnfreezeCharacter();
+        //if (unfreeze) GameManager.GM.player.GetComponent<GenericController>().UnfreezeCharacter();
         ContinueLoad();
     }
 
