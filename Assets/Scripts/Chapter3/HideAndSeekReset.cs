122fad3c944e0a9a1853d182522961b6bce5dc15
Eli-bassoon <61474669+Eli-bassoon@users.noreply.github.com>
2023-01-10 22:00:49 -0500

    Fixes to ch3

--- Assets/Scripts/Chapter3/HideAndSeekReset.cs
+++ Assets/Scripts/Chapter3/HideAndSeekReset.cs
@@ -2,12 +2,14 @@ using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class HideAndSeekReset : MonoBehaviour
 {
     [SerializeField] Transform principalReset;
     [SerializeField] Transform playerReset;
     [SerializeField] CinemachineVirtualCamera resetCam;
+    [SerializeField] UnityEvent onReset;
 
     public void ResetScene()
     {
@@ -31,7 +33,10 @@ public class HideAndSeekReset : MonoBehaviour
         // Camera
         GameManager.GM.ChangeActiveCam(resetCam.gameObject);
 
+        onReset.Invoke();
+
         // Scene
         GetComponent<SimpleTransitionSameScene>().Transition();
+
     }
 }
